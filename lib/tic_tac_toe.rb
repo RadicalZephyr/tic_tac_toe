cd86ddc07269cd337b03cc5025d2b383daf9fe23
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-13 15:55:30 -0500

    Actually store the board data

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,12 +3,19 @@ require "tic_tac_toe/version"
 module TicTacToe
   # Your code goes here...
   class Board
+
+    def initialize
+      @_marks = Array.new(9, " ")
+    end
+
     def pos index
-      " "
+      @_marks[index]
     end
 
     def move index, mark
-
+      @_marks[index] = mark
     end
+
   end
+
 end
