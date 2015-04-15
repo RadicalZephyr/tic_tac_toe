fd97bbfbc4a5f87e1a0f197d09281591b283d016
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-15 13:58:13 -0500

    Move board class into it's own file

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,45 +1,8 @@
 require "tic_tac_toe/version"
 require "tic_tac_toe/game"
+require "tic_tac_toe/board"
 
 module TicTacToe
 
-  class Board
-    WIN_PLACES = [[0, 1, 2],
-                  [3, 4, 5],
-                  [6, 7, 8],
-                  [0, 3, 6],
-                  [1, 4, 7],
-                  [2, 5, 8],
-                  [0, 4, 8],
-                  [2, 4, 6]]
-
-    def self.win_places
-      WIN_PLACES
-    end
-
-    def initialize
-      @_marks = Array.new(9, " ")
-    end
-
-    def pos index
-      @_marks[index]
-    end
-
-    def move(index, mark)
-      @_marks[index] = mark
-    end
-
-    def who_won?
-      WIN_PLACES.map do |places|
-        marks = places.map { |n| @_marks.at n }
-        marks[0] if marks.all? { |m| m == marks[0] } and marks[0] != " "
-      end.find(&:itself)
-    end
-
-    def draw?
-      (who_won? == nil) and @_marks.none? { |m| m == " " } ? true : false
-    end
-
-  end
 
 end
