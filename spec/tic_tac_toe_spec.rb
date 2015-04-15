26a78c2c4701af5d5ddd0fb0d4ef3d64daae6a57
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-15 14:01:16 -0500

    Move game spec into it's own file

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,24 +1,9 @@
 require 'spec_helper'
 
-class MockDisplay
-end
-
 describe TicTacToe do
 
   it 'has a version number' do
     expect(TicTacToe::VERSION).not_to be nil
   end
-
-  describe TicTacToe::Game do
-
-    it 'can start a game' do
-      display = MockDisplay.new
-      game = TicTacToe::Game.new display
-      game.start
-
-    end
-
-  end
-
 
 end
