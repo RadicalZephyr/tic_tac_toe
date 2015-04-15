3dcfd46a5e8d673e60100b658ead7785a99ba776
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-15 11:24:26 -0500

    Use let instead of before

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -21,10 +21,7 @@ describe TicTacToe do
   end
 
   describe TicTacToe::Board do
-
-    before :each do
-      @board = TicTacToe::Board.new
-    end
+    let(:board) { TicTacToe::Board.new }
 
     def set_board board, marks
       marks.each_with_index do |m, i|
@@ -34,21 +31,21 @@ describe TicTacToe do
 
     it 'initially has 9 empty positions' do
       (0..8).each do |i|
-          expect(@board.pos i).to eq(" ")
+          expect(board.pos i).to eq(" ")
       end
 
     end
 
     it 'can make moves' do
-      @board.move 0, "X"
-      expect(@board.pos 0).to eq("X")
+      board.move 0, "X"
+      expect(board.pos 0).to eq("X")
       (1..8).each do |i|
-        expect(@board.pos i).to eq(" ")
+        expect(board.pos i).to eq(" ")
       end
     end
 
     it 'only has indices 0..8' do
-      expect(@board.pos 10).to eq(nil)
+      expect(board.pos 10).to eq(nil)
     end
 
     def do_winning_test board, mark, places
@@ -69,24 +66,24 @@ describe TicTacToe do
     end
 
     it 'returns nil for non-won boards' do
-      expect(@board.who_won?).to eq nil
+      expect(board.who_won?).to eq nil
 
-      @board.move 0, "X"
-      @board.move 1, "X"
-      expect(@board.who_won?).to eq nil
+      board.move 0, "X"
+      board.move 1, "X"
+      expect(board.who_won?).to eq nil
 
-      @board.move 5, "X"
-      @board.move 8, "X"
-      expect(@board.who_won?).to eq nil
+      board.move 5, "X"
+      board.move 8, "X"
+      expect(board.who_won?).to eq nil
 
-      @board.move 6, "X"
-      expect(@board.who_won?).to eq nil
+      board.move 6, "X"
+      expect(board.who_won?).to eq nil
 
-      @board.move 2, "O"
-      @board.move 3, "O"
-      @board.move 4, "O"
-      @board.move 7, "O"
-      expect(@board.who_won?).to eq nil
+      board.move 2, "O"
+      board.move 3, "O"
+      board.move 4, "O"
+      board.move 7, "O"
+      expect(board.who_won?).to eq nil
     end
 
     it 'knows when its a draw' do
@@ -94,24 +91,24 @@ describe TicTacToe do
       # These are the only three basic draw positions in tic-tac-toe.
       # All others are simply rotations or inversions of these three
       # forms
-      set_board @board, ["X", "X", "O",
+      set_board board, ["X", "X", "O",
                          "O", "O", "X",
                          "X", "X", "O"]
-      expect(@board.draw?).to eq true
+      expect(board.draw?).to eq true
 
-      set_board @board, ["X", "X", "O",
+      set_board board, ["X", "X", "O",
                          "O", "X", "X",
                          "X", "O", "O"]
-      expect(@board.draw?).to eq true
+      expect(board.draw?).to eq true
 
-      set_board @board, ["X", "X", "O",
+      set_board board, ["X", "X", "O",
                          "O", "O", "X",
                          "X", "O", "x"]
-      expect(@board.draw?).to eq true
+      expect(board.draw?).to eq true
     end
 
     it 'doesn\'t report false positive draws' do
-      expect(@board.draw?).to eq false
+      expect(board.draw?).to eq false
     end
 
   end
