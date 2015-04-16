b5017cb1d7a50c0539d474d84efb3d8c9a729bc7
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-16 17:23:51 -0500

    Add a function for wiring up and starting a game

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,5 +3,9 @@ require "tic_tac_toe/game"
 require "tic_tac_toe/board"
 
 module TicTacToe
-
+  def TicTacToe.run
+    g = TicTacToe::Game.new(IO::console, TicTacToe::Board.new)
+    g.start
+    g.game_loop
+  end
 end
