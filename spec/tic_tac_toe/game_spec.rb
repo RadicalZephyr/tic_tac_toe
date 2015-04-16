99aee1a783dceb850f9850bff15132382ca7948b
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-16 10:56:27 -0500

    Add a let for the display test variable

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -4,18 +4,16 @@ class MockDisplay
 end
 
 describe TicTacToe::Game do
+  let(:display) { double(:display) }
 
   it 'can start a game' do
-    display = double(:display)
     allow(display).to receive(:printf)
-
     game = TicTacToe::Game.new display
     game.start
   end
 
   describe 'when playing' do
     it 'changes the active player' do
-      display = double(:display)
       game = TicTacToe::Game.new display
 
       expect { game.do_turn }.to change { game.to_play }
