ce1e436d4f25c8000d8c516bd0142c0b2bcc8ccc
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-16 17:39:33 -0500

    Write a board_to_string function
    fixup! Write a print_board function

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -18,6 +18,7 @@ describe TicTacToe::Game do
   def mock_board
     instance_double('TicTacToe::Board').tap do |board|
       allow(board).to receive(:move)
+      allow(board).to receive(:board_to_string)
       allow(board).to receive(:legal?).and_return(true)
     end
   end
