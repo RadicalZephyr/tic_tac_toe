2fd181b1372d3cbea05f14999fd296a5c9c7a555
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-16 10:57:25 -0500

    Add a let for the game object

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -5,17 +5,15 @@ end
 
 describe TicTacToe::Game do
   let(:display) { double(:display) }
+  let(:game) { TicTacToe::Game.new display }
 
   it 'can start a game' do
     allow(display).to receive(:printf)
-    game = TicTacToe::Game.new display
     game.start
   end
 
   describe 'when playing' do
     it 'changes the active player' do
-      game = TicTacToe::Game.new display
-
       expect { game.do_turn }.to change { game.to_play }
     end
   end
