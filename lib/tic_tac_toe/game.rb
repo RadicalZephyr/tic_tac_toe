604ce4f3309ab482a88beb1acadb383116ceef28
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-16 10:53:17 -0500

    Write a very basic do_turn method

--- lib/tic_tac_toe/game.rb
+++ lib/tic_tac_toe/game.rb
@@ -1,14 +1,19 @@
 module TicTacToe
   class Game
+    attr_reader :to_play
 
     def initialize display
       @display = display
+      @to_play = "X"
     end
 
     def start
       @display.printf "Welcome to Tic Tac Toe"
     end
 
+    def do_turn
+      @to_play = "O"
+    end
   end
 
 end
