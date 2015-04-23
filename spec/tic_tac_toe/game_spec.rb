8bde7e38ecef0352d9933a3bb2b08417335c5127
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-23 14:32:20 -0500

    Don't mock the inject board in Game tests

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -2,16 +2,8 @@ require 'spec_helper'
 
 describe TicTacToe::Game do
 
-  let(:board) { mock_board }
+  let(:board) { TicTacToe::Board.new }
   let(:game)  { TicTacToe::Game.new(board) }
-
-  def mock_board
-    instance_double('TicTacToe::Board').tap do |board|
-      allow(board).to receive(:move)
-      allow(board).to receive(:board_to_string)
-      allow(board).to receive(:legal?).and_return(true)
-    end
-  end
 
   context 'when playing' do
     it 'changes the active player after every move' do
@@ -19,25 +11,10 @@ describe TicTacToe::Game do
       expect { game.try_move(index: 2) }.to change { game.current_mark }
     end
 
-    it 'changes the board marks based on user input' do
-      expect(board).to receive(:move).with(anything, 1)
-      game.try_move(index: 1)
-    end
-
     it "Doesn't allow the same move twice." do
-      allow(board).to receive(:legal?).and_return(true, false, true)
-      expect(board).to receive(:move).with(anything, 1)
-      game.try_move(index: 1)
-      game.try_move(index: 1)
-      game.try_move(index: 2)
-    end
-
-    it 'Keeps reading until it gets a legal move' do
-      allow(board).to receive(:legal?).and_return(false, false, true)
-      expect(board).to receive(:move).with(anything, 1)
-      game.try_move(index: 8)
-      game.try_move(index: 3)
-      game.try_move(index: 1)
+      expect(game.try_move(index: 1)).to eq(true)
+      expect(game.try_move(index: 1)).to eq(false)
+      expect(game.try_move(index: 2)).to eq(true)
     end
 
   end
