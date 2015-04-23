868554b52f52341e0c1b691972104ed2a0155acd
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-23 11:48:39 -0500

    Rename Game class to Console

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,11 +1,11 @@
 require "tic_tac_toe/version"
-require "tic_tac_toe/game"
+require "tic_tac_toe/console"
 require "tic_tac_toe/board"
 require "tic_tac_toe/console_shell"
 
 module TicTacToe
   def TicTacToe.run
-    g = TicTacToe::Game.new(IO::console, TicTacToe::Board.new)
+    g = TicTacToe::Console.new(IO::console, TicTacToe::Board.new)
     g.game_loop
   end
 end
