d3fb641c1b89de229563b72aa31b2d7c4a53bdbe
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-23 11:55:07 -0500

    Add a stub Game class

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 require "tic_tac_toe/version"
 require "tic_tac_toe/console"
 require "tic_tac_toe/board"
-require "tic_tac_toe/console_shell"
+require "tic_tac_toe/game"
 
 module TicTacToe
   def TicTacToe.run
