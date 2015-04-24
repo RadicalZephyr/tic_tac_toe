beb38f66c103c773a904bdb7de46df92e1590f17
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-24 14:07:42 -0500

    Add an integration test that plays a game with a winner

---
+++ spec/end_to_end_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'io/console'
+
+describe "Playing a full game" do
+  let(:console) { mock_console }
+  let(:board)   { TicTacToe::Board.new }
+  let(:game)    { TicTacToe::Game.new(board) }
+  let(:shell)   { TicTacToe::Console.new(console, game) }
+
+  def mock_console
+    instance_double('IO').tap do |console|
+      allow(console).to receive(:puts)
+      allow(console).to receive(:print)
+      allow(console).to receive(:flush)
+    end
+  end
+
+  it "ends after a win" do
+    x_moves = [0, 1, 2]
+    y_moves = [3, 4]
+    moves = x_moves.zip(y_moves).flatten
+    inputs = moves.map { |move| move.to_s + "\n" }
+    allow(console).to receive(:gets).and_return(*inputs)
+    shell.game_loop
+  end
+
+end
