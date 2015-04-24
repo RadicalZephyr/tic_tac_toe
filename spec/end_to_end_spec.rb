4c4c076990549b57b088834fc870ae01f58fc7d6
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-24 14:20:18 -0500

    Extract method to deduplicate code

--- spec/end_to_end_spec.rb
+++ spec/end_to_end_spec.rb
@@ -15,21 +15,24 @@ describe "Playing a full game" do
     end
   end
 
-  it "ends after a win" do
-    x_moves = [0, 1, 2]
-    y_moves = [3, 4]
+  def with_moves(x_moves:, y_moves:)
     moves = x_moves.zip(y_moves).flatten
     inputs = moves.map { |move| move.to_s + "\n" }
     allow(console).to receive(:gets).and_return(*inputs)
+  end
+
+  it "ends after a win" do
+    x_moves = [0, 1, 2]
+    y_moves = [3, 4]
+    with_moves(x_moves: x_moves, y_moves: y_moves)
     shell.game_loop
   end
 
   it "ends after a draw" do
     x_moves = [0, 1, 3, 8, 4]
     y_moves = [2, 6, 7, 5]
-    moves = x_moves.zip(y_moves).flatten
-    inputs = moves.map { |move| move.to_s + "\n" }
-    allow(console).to receive(:gets).and_return(*inputs)
+    with_moves(x_moves: x_moves, y_moves: y_moves)
     shell.game_loop
   end
+
 end
