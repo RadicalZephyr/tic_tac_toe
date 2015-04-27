3a81cfe1cd6c79063b3fa0134883e89485a8c2e9
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-27 12:05:26 -0500

    Reactivate multi-game integration test
    There were three problems it seems.
    
    1. First, expecting a method to be called actually mocks it, causing it
       to not be real unless you explicitly say to call the original.
    2. I wasn't compacting the input list, thus there was a potential for
       nil inputs
    3. Finally, the code problem which was not having a way to reset the
       board.

--- spec/end_to_end_spec.rb
+++ spec/end_to_end_spec.rb
@@ -39,13 +39,13 @@ describe "Playing a full game" do
     shell.game_loop
   end
 
-  xit 'plays multiple games from main loop' do
+  it 'plays multiple games from main loop' do
     x_moves = [0, 1, 3, 8, 4]
     y_moves = [2, 6, 7, 5]
     moves = [make_moves(x_moves, y_moves)] * 2
-    inputs = moves.zip(["y\n", "n\n"]).flatten
+    inputs = moves.zip(["y\n", "n\n"]).flatten.compact
     expect(console).to receive(:gets).and_return(*inputs)
-    expect(shell).to receive(:game_loop).twice
+    expect(shell).to receive(:game_loop).twice.and_call_original
     shell.main_loop
   end
 end
