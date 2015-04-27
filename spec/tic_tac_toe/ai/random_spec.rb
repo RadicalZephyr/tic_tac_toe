89106ca4e5601c34ae78e4f850a8cae1f5ebab00
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-27 13:36:03 -0500

    Fill out the test of the random AI

--- spec/tic_tac_toe/ai/random_spec.rb
+++ spec/tic_tac_toe/ai/random_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe TicTacToe::AI::Random do
+  let(:randomness) { instance_double("Random") }
+  let(:board) { double }
+  let(:rando) { TicTacToe::AI::Random.new(randomness)}
 
+  it 'Returns random moves' do
+    allow(randomness).to receive(:rand).with(8).and_return(1, 2, 3)
+    expect(rando.get_move(board)).to eq(1)
+    expect(rando.get_move(board)).to eq(2)
+    expect(rando.get_move(board)).to eq(3)
+  end
 end
