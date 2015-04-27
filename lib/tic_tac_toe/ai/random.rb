0aa27a8771b09076951175e0c2712497fb45fd83
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-27 13:36:12 -0500

    Complete the random AI move generator

--- lib/tic_tac_toe/ai/random.rb
+++ lib/tic_tac_toe/ai/random.rb
@@ -2,6 +2,14 @@ module TicTacToe
   module AI
 
     class Random
+
+      def initialize(random_source)
+        @random_source = random_source
+      end
+
+      def get_move(board)
+        @random_source.rand(8)
+      end
     end
 
   end
