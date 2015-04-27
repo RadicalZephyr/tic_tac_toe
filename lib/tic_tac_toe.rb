eb333d8467aa608040a9b7328d6b8d5363a169e2
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-27 13:23:42 -0500

    Add a stub test for the simple AI.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,8 +3,10 @@ require "tic_tac_toe/console_shell"
 require "tic_tac_toe/board"
 require "tic_tac_toe/game"
 require "tic_tac_toe/rules"
+require "tic_tac_toe/ai/random"
 
 module TicTacToe
+
   def TicTacToe.run
     game = TicTacToe::Game.new(TicTacToe::Board.new)
     shell = TicTacToe::ConsoleShell.new(IO::console, game)
---
+++ spec/tic_tac_toe/ai/random_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe TicTacToe::AI::Random do
+
+end
