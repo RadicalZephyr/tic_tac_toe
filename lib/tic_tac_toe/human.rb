acea155702e6908dc21ddfaa8855078b0c33160d
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-27 14:21:05 -0500

    Extract the get_move functionality into a separate class

---
+++ lib/tic_tac_toe/human.rb
@@ -0,0 +1,29 @@
+module TicTacToe
+  class Human
+    attr_reader :shell
+
+    def initialize(shell)
+      @shell = shell
+    end
+
+    def get_move
+      got_input = false
+      while not got_input
+        str = shell.prompt_move
+        begin
+          input = Integer(str)
+          if input.between?(0,8)
+            got_input = true
+          else
+            shell.show_invalid_move_message
+          end
+        rescue ArgumentError
+          shell.show_move_error_message
+        end
+      end
+
+      input
+    end
+
+  end
+end
