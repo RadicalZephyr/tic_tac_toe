33b5b9ecc2c9cecd15cbd97df7cd3dac689bfebc
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-28 11:58:32 -0500

    Implement win detection as simply as possible

--- lib/tic_tac_toe/ai/minimax.rb
+++ lib/tic_tac_toe/ai/minimax.rb
@@ -7,8 +7,22 @@ module TicTacToe
         @my_mark = my_mark
       end
 
+      def count_marks(marks)
+        marks.each_with_object(Hash.new(0)) { |mark, counts| counts[mark] += 1 }
+      end
+
+      def i_can_win?(imarks)
+        counts = count_marks(imarks.map { |imark| imark[1] })
+        counts[@my_mark] == 2 && counts[" "] == 1
+      end
+
+      def empty_space(imarks)
+        imarks.select { |imark| imark[1] == " " }.map { |imark| imark[0] }
+      end
+
       def get_move(board)
-        2
+        attacks = board.indexed_attack_sets
+        attacks.map { |imarks| empty_space(imarks) if i_can_win?(imarks)  }.flatten.compact.first
       end
 
     end
