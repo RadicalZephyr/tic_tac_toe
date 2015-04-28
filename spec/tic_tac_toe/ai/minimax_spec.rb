2ebe12ff8c74cb78993457653866c818b831330f
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-28 11:18:20 -0500

    Add some basic move tests

--- spec/tic_tac_toe/ai/minimax_spec.rb
+++ spec/tic_tac_toe/ai/minimax_spec.rb
@@ -1,9 +1,29 @@
 require 'spec_helper'
 
 describe TicTacToe::AI::Minimax do
-  it 'wins when it has two in a row' do
-    board = TicTacToe::Board.from([" ", " ", " ",
-                                   " ", " ", " ",
-                                   " ", " ", " "])
+  let(:ai) { TicTacToe::AI::Minimax.new }
+
+  describe 'wins when it has two in a row' do
+    it 'on the left' do
+      board = TicTacToe::Board.from([" ", "X", "X",
+                                     " ", " ", " ",
+                                     " ", " ", " "])
+      expect(ai.get_move(board)).to eq(0)
+    end
+
+    it 'in the center' do
+      board = TicTacToe::Board.from(["X", " ", "X",
+                                     " ", " ", " ",
+                                     " ", " ", " "])
+      expect(ai.get_move(board)).to eq(1)
+    end
+
+    it 'on the right' do
+      board = TicTacToe::Board.from(["X", "X", " ",
+                                     " ", " ", " ",
+                                     " ", " ", " "])
+      expect(ai.get_move(board)).to eq(2)
+    end
+
   end
 end
