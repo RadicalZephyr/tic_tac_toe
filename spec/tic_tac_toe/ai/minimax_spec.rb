d3a1d3fb36cdff079d319e460767cc05dd68ba72
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-28 11:03:57 -0500

    Force board to take a set of marks in it's constructor

--- spec/tic_tac_toe/ai/minimax_spec.rb
+++ spec/tic_tac_toe/ai/minimax_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe TicTacToe::AI::Minimax do
-
+  it 'wins when it has two in a row' do
+    board = TicTacToe::Board.from([" ", " ", " ",
+                                   " ", " ", " ",
+                                   " ", " ", " "])
+  end
 end
