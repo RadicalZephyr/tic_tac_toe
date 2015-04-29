01d1b2697f1a2ca66491ea3007a8a75ac2041827
Zefira Shannon <earthlingzephyr@gmail.com>
2015-04-29 15:28:34 -0500

    Remove the no longer used try_move method

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -6,16 +6,6 @@ describe TicTacToe::Game do
   let(:game)  { TicTacToe::Game.make_new_game(board) }
 
   context 'when playing' do
-    it 'changes the active player after every move' do
-      expect { game.try_move(index: 1) }.to change { game.current_mark }
-      expect { game.try_move(index: 2) }.to change { game.current_mark }
-    end
-
-    it "Doesn't allow the same move twice." do
-      expect(game.try_move(index: 1)).to eq(true)
-      expect(game.try_move(index: 1)).to eq(false)
-      expect(game.try_move(index: 2)).to eq(true)
-    end
 
     it 'changes the active player after every legal move' do
       expect { game.move(index: 1) }.to change { game.current_mark }
