8d3ccd20bcf40e3f23611909cec8e2ff0bdeff94
Geoff Shannon <geoffpshannon@gmail.com>
2015-04-29 15:17:51 -0500

    Write a pure command version of move
    Instead of a return value, it raises an argument error on invalid moves

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -17,5 +17,16 @@ describe TicTacToe::Game do
       expect(game.try_move(index: 2)).to eq(true)
     end
 
+    it 'changes the active player after every legal move' do
+      expect { game.move(index: 1) }.to change { game.current_mark }
+      expect { game.move(index: 2) }.to change { game.current_mark }
+    end
+
+    it "doesn't allow the same move twice." do
+      game.move(index: 1)
+      expect { game.move(index:1) }.to raise_error
+      game.move(index: 2)
+    end
+
   end
 end
