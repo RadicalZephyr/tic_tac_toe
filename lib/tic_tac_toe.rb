cc9667243ea333f44ec9b3e958a90a42f05eefe6
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-01 16:25:12 -0500

    Stub out a new Mark class

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,7 @@
 require "tic_tac_toe/version"
 require "tic_tac_toe/console_shell"
 require "tic_tac_toe/board"
+require "tic_tac_toe/mark"
 require "tic_tac_toe/game"
 require "tic_tac_toe/rules"
 require "tic_tac_toe/ai/random"
