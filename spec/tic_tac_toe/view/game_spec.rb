acdc55ddc158edbe0654f0db895e46e2966949c6
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-05 16:11:23 -0500

    Add a view integration test
    All it cares about is that the render call doesn't throw.

--- spec/tic_tac_toe/view/game_spec.rb
+++ spec/tic_tac_toe/view/game_spec.rb
@@ -3,4 +3,9 @@ require 'tic_tac_toe/view/game'
 
 describe TicTacToe::View::Game do
 
+  it 'can find the mustache template' do
+    gv = TicTacToe::View::Game.new(TicTacToe::Game.make_new_game(TicTacToe::Board.empty_board))
+    gv.render
+  end
+
 end
