7db0625abb0907ce8b4f6565cdfdb10eab92cee8
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-05 14:47:17 -0500

    Fill out view classes for mustache templates

--- lib/tic_tac_toe/views/board.rb
+++ lib/tic_tac_toe/views/board.rb
@@ -4,7 +4,29 @@ module TicTacToe
   module Views
 
     class Board < Mustache
+      attr_reader :board
+
+      self.template_path = File.dirname(__FILE__)
+
+      def initialize(board)
+        @board = board
+      end
+
       def positions
+        board.map.with_index do |mark, index|
+          {:mark => mark,
+           :index => index,
+           :beginrow => is_row_start?(index),
+           :endrow =>   is_row_end?(index)}
+        end
+      end
+
+      def is_row_start(index)
+        (index % 3) == 0
+      end
+
+      def is_row_end(index)
+        (index % 3) == 2
       end
     end
 
--- lib/tic_tac_toe/views/game.rb
+++ lib/tic_tac_toe/views/game.rb
@@ -1,22 +1,43 @@
 require 'mustache'
+require 'tic_tac_toe/views/board'
 
 module TicTacToe
   module Views
 
     class Game < Mustache
+      attr_reader :game, :x_mark, :o_mark
+
+      self.template_path = File.dirname(__FILE__)
+
+      def initialize(game)
+        @game = game
+        @x_mark = TicTacToe::Game::X
+        @o_mark = TicTacToe::Game::O
+      end
+
       def post_url
+        # TODO: this
       end
 
-      def active_mark
+      def current_mark
+        case game.current_mark
+        when x_mark
+          "ex"
+        when o_mark
+          "oh"
+        end
       end
 
-      def x_active
+      def x_current
+        game.current_mark == x_mark
       end
 
-      def o_active
+      def o_current
+        game.current_mark == o_mark
       end
 
       def board
+        TicTacToe::Views::Board.new(game.board).render
       end
     end
 
