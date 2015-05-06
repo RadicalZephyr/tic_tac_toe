1093f9c7e793ba77c8113bf989ff446db2fc08d2
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-06 17:09:04 -0500

    Stub out the move route processing

--- lib/tic_tac_toe/rack_shell.rb
+++ lib/tic_tac_toe/rack_shell.rb
@@ -11,8 +11,13 @@ module TicTacToe
 
     def call(env)
       if env["PATH_INFO"] =~ %r{^/new$}
-        @game = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
-        [200, {}, [TicTacToe::View::Game.new(@game).render]]
+        game = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
+        [200, {}, [TicTacToe::View::Game.new(game).render]]
+      elsif env["REQUEST_METHOD"] == "POST" && env["PATH_INFO"] =~ %r{^/move$}
+        # Create the game from the game form parameter
+        # process the move from the move form parameter
+        game = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
+        [200, {}, [TicTacToe::View::Game.new(game).render]]
       else
         [404, {}, ["Not found."]]
       end
