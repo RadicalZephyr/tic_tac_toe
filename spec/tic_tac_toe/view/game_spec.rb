3a733c58903232e1b85b8237f4017533a4bde27a
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-06 15:33:30 -0500

    Add a view test for current_mark

--- spec/tic_tac_toe/view/game_spec.rb
+++ spec/tic_tac_toe/view/game_spec.rb
@@ -8,4 +8,12 @@ describe TicTacToe::View::Game do
     expect(gv.render).to be_a(String).and start_with('<!DOCTYPE html>').and end_with("</html>\n")
   end
 
+  it 'returns the correct value for the current mark image' do
+    g = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
+    gv = TicTacToe::View::Game.new(g)
+    expect(gv.current_mark).to eq("ex")
+    g.move(index: 0)
+    expect(gv.current_mark).to eq("oh")
+  end
+
 end
