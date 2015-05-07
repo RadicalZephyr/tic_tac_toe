143a5c6e8cc4f05e73a1784c449933325829949c
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-07 14:20:15 -0500

    Make Game#move private

--- spec/tic_tac_toe/game_spec.rb
+++ spec/tic_tac_toe/game_spec.rb
@@ -2,21 +2,32 @@ require 'spec_helper'
 
 describe TicTacToe::Game do
 
+  let(:player) { mock_player }
   let(:board) { TicTacToe::Board.empty_board }
-  let(:game)  { TicTacToe::Game.new_game(board) }
+  let(:game) { test_game }
 
-  context 'when playing' do
-
-    it 'changes the active player after every legal move' do
-      expect { game.move(index: 1) }.to change { game.current_mark }
-      expect { game.move(index: 2) }.to change { game.current_mark }
+  def mock_player
+    instance_double("TicTacToe::Human").tap do |player|
+      allow(player).to receive(:set_marks)
+      allow(player).to receive(:get_move).and_return(0)
     end
-
-    it "doesn't allow the same move twice." do
-      game.move(index: 1)
-      expect { game.move(index:1) }.to raise_error
-      game.move(index: 2)
-    end
-
   end
+
+  def test_game
+    TicTacToe::Game.new_game(board).tap do |game|
+      game.set_players(player, player)
+    end
+  end
+
+  it "swaps the mark after every turn" do
+    allow(player).to receive(:get_move).and_return(0, 1)
+    expect { game.next_turn }.to change { game.current_mark }
+    expect { game.next_turn }.to change { game.current_mark }
+  end
+
+  it "doesn't allow the same move to happen twice" do
+    game.next_turn
+    expect { game.next_turn }.to raise_error
+  end
+
 end
