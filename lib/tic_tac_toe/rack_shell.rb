02912e6a9735a7d254843bc911916f11cf0dfcf7
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-07 10:46:44 -0500

    Remove spiked out code

--- lib/tic_tac_toe/rack_shell.rb
+++ lib/tic_tac_toe/rack_shell.rb
@@ -16,19 +16,6 @@ module TicTacToe
 
       if req.path =~ %r{^/$}
         [200, {}, ["Hello, and welcome to Tic-Tac-Toe!"]]
-      elsif req.path =~ %r{^/new$}
-        game = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
-        [200, {}, [TicTacToe::View::Game.new(game).render]]
-
-      elsif req.post? && req.path =~ %r{^/move$}
-        game_hash = JSON.parse(req["game"])
-        move_index = Integer(req["move"])
-        board = TicTacToe::Board.from(game_hash["marks"])
-        game = TicTacToe::Game.new(board)
-        game.current_mark= TicTacToe::Mark.new(game_hash["current_mark"])
-        game.move(index: move_index)
-        [200, {}, [TicTacToe::View::Game.new(game).render]]
-
       else
         [404, {}, ["Not found."]]
       end
