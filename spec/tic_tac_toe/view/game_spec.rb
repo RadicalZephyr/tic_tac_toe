c58457b9b5ace8af95ec1270f922ecc80f449ef7
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-07 10:18:33 -0500

    Extract view setup in GameView spec

--- spec/tic_tac_toe/view/game_spec.rb
+++ spec/tic_tac_toe/view/game_spec.rb
@@ -2,17 +2,16 @@ require 'spec_helper'
 require 'tic_tac_toe/view/game'
 
 describe TicTacToe::View::Game do
+  let(:game) { TicTacToe::Game.new_game(TicTacToe::Board.empty_board) }
+  let(:gv) { TicTacToe::View::Game.new(game) }
 
   it 'can find the mustache template' do
-    gv = TicTacToe::View::Game.new(TicTacToe::Game.new_game(TicTacToe::Board.empty_board))
     expect(gv.render).to be_a(String).and start_with('<!DOCTYPE html>').and end_with("</html>\n")
   end
 
   it 'returns the correct value for the current mark image' do
-    g = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
-    gv = TicTacToe::View::Game.new(g)
     expect(gv.current_mark).to eq("ex")
-    g.move(index: 0)
+    game.move(index: 0)
     expect(gv.current_mark).to eq("oh")
   end
 
