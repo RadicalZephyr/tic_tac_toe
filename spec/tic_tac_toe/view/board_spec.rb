f0a9454cd449488eb40bf88f5500b1fe2220b2df
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-07 10:17:02 -0500

    Extract view setup in BoardView spec

--- spec/tic_tac_toe/view/board_spec.rb
+++ spec/tic_tac_toe/view/board_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 require 'tic_tac_toe/view/board'
 
 describe TicTacToe::View::Board do
+  let(:bv) { TicTacToe::View::Board.new(TicTacToe::Board.empty_board) }
 
   it 'Properly denotes row starts and ends' do
-    bv = TicTacToe::View::Board.new(TicTacToe::Board.empty_board)
     starts = (0...9).map { |i| bv.is_row_start?(i) }
     ends   = (0...9).map { |i| bv.is_row_end?(i) }
     expect(starts).to eq([true, false, false,
@@ -16,15 +16,14 @@ describe TicTacToe::View::Board do
   end
 
   it 'Returns correct position data' do
-    bv = TicTacToe::View::Board.new(TicTacToe::Board.empty_board)
     expect(bv.positions).to include({:blank? => true, :mark => " ", :index => 0,
                                      :beginrow => true, :endrow => false})
   end
 
   it 'Renders a semblance of a board' do
-    bv = TicTacToe::View::Board.new(TicTacToe::Board.empty_board)
     result = bv.render
     expect(result).to be_a(String).and start_with("  <table>").and end_with("  </table>\n")
     expect(result).to include("<tr>").and include("</tr>").and include("<td>").and include("</td>")
   end
+
 end
