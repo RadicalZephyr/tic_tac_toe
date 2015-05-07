0b6ad7a59cd1a768701f85e9a14b23255190fd5c
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-07 12:19:41 -0500

    Remove tests for soon-to-be private methods

--- spec/tic_tac_toe/console_spec.rb
+++ spec/tic_tac_toe/console_spec.rb
@@ -26,63 +26,6 @@ describe TicTacToe::ConsoleShell do
     end
   end
 
-  describe 'when playing' do
-
-    it 'makes a move based on user input' do
-      expect(game).to receive(:move).with({:index => 1})
-      console_shell.do_turn
-    end
-
-    it 'keeps reading until it gets a legal move' do
-      allow(console).to receive(:gets).and_return("1\n", "1\n", "2\n")
-      allow(game).to receive(:move).with({:index => 1}).and_raise(ArgumentError)
-      allow(game).to receive(:move).with({:index => 2})
-      console_shell.do_turn
-    end
-
-  end
-
-  describe "handling yes or no questions" do
-    it "handles y or yes as true" do
-      ["y", "yes"].each do |text|
-        expect(console_shell.yn_to_bool(text)).to be_truthy
-      end
-    end
-
-    it "handles n or no as false" do
-      ["n", "no"].each do |text|
-        expect(console_shell.yn_to_bool(text)).to be_falsey
-      end
-    end
-
-    it "handles anything else as nil" do
-      ["ab", "euonth" "8oe02b"].each do |text|
-        expect(console_shell.yn_to_bool(text)).to be_nil
-      end
-    end
-  end
-
-  describe 'when running the game loop' do
-    it 'keeps running while the board is not complete' do
-      allow(game).to receive(:move)
-      expect(game).to receive(:finished?).exactly(3).times.and_return(false, false, true)
-      console_shell.game_loop
-    end
-  end
-
-  describe 'choosing players' do
-    it 'can return two human players' do
-      allow(console).to receive(:gets).and_return("h\n", "h\n")
-      expect(console_shell.choose_players).to include(player, player)
-    end
-
-    it 'can return one human and one ai player' do
-      allow(console).to receive(:gets).and_return("h\n", "r\n")
-      expect(console_shell.choose_players).to include(player,
-                                                      an_instance_of(TicTacToe::AI::Random))
-    end
-  end
-
   describe 'running the console shell' do
 
     it 'it only plays one game if the user says no' do
