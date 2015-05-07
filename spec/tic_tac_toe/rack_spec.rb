e65fcc05dceb7fed1315dacc05990c405d7423a7
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-07 10:15:01 -0500

    Add a basic MockRequest test for RackShell

--- spec/tic_tac_toe/rack_spec.rb
+++ spec/tic_tac_toe/rack_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
+require 'rack'
+require 'tic_tac_toe/rack_shell'
 
 describe TicTacToe::RackShell do
+
+  it 'can receive an index/root GET request' do
+    app = TicTacToe::RackShell.new_shell
+    req = Rack::MockRequest.new(app)
+    response = req.get("/")
+    expect(response.accepted?).to be_truthy
+  end
 
 end
