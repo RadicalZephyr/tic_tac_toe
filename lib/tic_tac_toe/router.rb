72b20a48611f4ee995519158a1f1aa64264b8f7f
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-13 16:25:22 -0500

    Add the simplest router implementation

--- lib/tic_tac_toe/router.rb
+++ lib/tic_tac_toe/router.rb
@@ -1,6 +1,17 @@
 module TicTacToe
 
   class Router
+
+    def add_route(path, method, view, &block)
+      puts block.inspect
+      @block = block
+      @view = view
+    end
+
+    def match(env)
+      [200, {}, [@view.render(@block.call(env))]]
+    end
+
   end
 
 end
--- spec/tic_tac_toe/router_spec.rb
+++ spec/tic_tac_toe/router_spec.rb
@@ -2,5 +2,23 @@ require 'spec_helper'
 require 'rack_tac_toe'
 
 describe TicTacToe::Router do
+  include Rack::Test::Methods
+
+  let(:router) { TicTacToe::Router.new }
+
+  def app
+    Proc.new do |env|
+      router.match(env)
+    end
+  end
+
+  it 'can add specific routes' do
+    view = double
+    data = {:dummy => :data}
+    router.add_route("/", :GET, view) { |req| data }
+    expect(view).to receive(:render).with(data)
+
+    get '/'
+  end
 
 end
