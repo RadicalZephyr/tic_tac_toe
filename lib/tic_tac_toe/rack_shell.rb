da6eceecc624521aff360d3922227569e2217d59
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-13 15:05:41 -0500

    Add a high-level test for the new game route

--- lib/tic_tac_toe/rack_shell.rb
+++ lib/tic_tac_toe/rack_shell.rb
@@ -16,6 +16,9 @@ module TicTacToe
 
       if req.path =~ %r{^/$}
         [200, {}, ["Hello, and welcome to Tic-Tac-Toe!"]]
+      elsif req.path =~ %r{^/new-game$}
+        game = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
+        [200, {}, [TicTacToe::View::Game.new(game).render]]
       else
         [404, {}, ["Not found."]]
       end
