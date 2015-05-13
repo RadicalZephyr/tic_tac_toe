e58296b86ce4da2ac362cb31b577645a7abdceb9
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-13 17:46:54 -0500

    Make requests specific to method and path

--- lib/tic_tac_toe/router.rb
+++ lib/tic_tac_toe/router.rb
@@ -4,15 +4,17 @@ module TicTacToe
     attr_reader :routes
 
     def initialize
-      @routes = Hash.new({})
+      @routes = {}
     end
 
     def add_route(path, method, view, &block)
-      routes[path] = {:view => view, :block => block}
+      routes[{:path => path,
+              :method => method.to_s}] = {:view => view, :block => block}
     end
 
     def match(env)
-      route = routes[env["PATH_INFO"]]
+      route = routes[{:path => env["PATH_INFO"],
+                      :method => env["REQUEST_METHOD"]}]
       [200, {}, [route[:view].render(route[:block].call(env))]]
     end
 
