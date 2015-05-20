f44475f4f3eca60aa2d7b50c0be9c588d2590bce
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-20 12:06:47 -0500

    Add the ability to choose different players

--- lib/tic_tac_toe/rack_shell.rb
+++ lib/tic_tac_toe/rack_shell.rb
@@ -7,15 +7,16 @@ module TicTacToe
 
   class RackShell
     attr_accessor :req
+
     def self.new_shell
       router, player = TicTacToe::Router.new, TicTacToe::Human.new
-      shell = RackShell.new(router)
+      shell = RackShell.new(router, player)
 
       router.add_route("/", :GET, TicTacToe::View::Home) { |_| nil }
-      router.add_route("/new-game", :GET, TicTacToe::View::Game) do |env|
+      router.add_route("/new-game", :POST, TicTacToe::View::Game) do |env|
         req = Rack::Request.new(env)
         game = TicTacToe::Game.new_game(TicTacToe::Board.empty_board)
-        game.set_players(player, player)
+        game.set_players(*shell.get_players(req))
         req.session[:game] = game
 
         game
@@ -36,8 +37,8 @@ module TicTacToe
       return shell
     end
 
-    def initialize(router)
-      @router = router
+    def initialize(router, player)
+      @router, @player = router, player
     end
 
     def get_move
@@ -47,6 +48,23 @@ module TicTacToe
     def call(env)
       @router.match(env)
     end
-  end
 
+    def get_players(req)
+      [get_player(req["player1"]),
+       get_player(req["player2"])]
+    end
+
+    private
+    def get_player(type)
+      case type
+      when "h", "human"
+        return @player
+      when "r", "random"
+        return TicTacToe::AI::Random.new(Random.new)
+      when "a", "ai"
+        return TicTacToe::AI::Minimax.new
+      end
+    end
+
+  end
 end
--- lib/tic_tac_toe/view/home.rb
+++ lib/tic_tac_toe/view/home.rb
@@ -9,6 +9,13 @@ module TicTacToe
         self.new.render
       end
 
+      def players
+        base_player = {:types => [{:type => "human", :type_value => "h"},
+                                  {:type => "ai", :type_value => "a"},
+                                  {:type => "random", :type_value => "r"}]}
+        [{:index => 1}.merge(base_player),
+         {:index => 2}.merge(base_player)]
+      end
     end
 
   end
