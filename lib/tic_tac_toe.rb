1112a6013a0a3e5109b4f43b5c834f8a11b242fc
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-27 13:26:23 -0500

    Fully remove the unused Human player class

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,7 +4,6 @@ require "tic_tac_toe/core/board"
 require "tic_tac_toe/core/mark"
 require "tic_tac_toe/core/game"
 require "tic_tac_toe/core/rules"
-require "tic_tac_toe/core/players/human"
 require "tic_tac_toe/core/players/random"
 require "tic_tac_toe/core/players/minimax"
 
