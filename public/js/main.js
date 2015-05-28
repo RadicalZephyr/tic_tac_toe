31df8f9921acace9d5ba83f0855f9079d9b55169
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-28 13:57:51 -0500

    Wire the js classes together in main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,61 +1,11 @@
-function disableOptions() {
-  for (var i = 0; i < 9; i++) {
-    options[i].disabled = true;
-  }
+var submitBtn = document.getElementById("submit-btn");
+
+var options = [];
+
+for (var i = 0; i < 9; i++) {
+    var opt = document.getElementById("move"+i);
+    options.push(new GridSquare(opt));
 }
 
-function enableOptions() {
-  for (var i = 0; i < 9; i++) {
-    options[i].disabled = false;
-  }
-}
-
-function setNewBoard(board) {
-  for (var i = 0; i < 9; i++) {
-    if (board[i] !== " ") {
-      options[i].parentElement.classList.add(board[i].toUpperCase()+"-marker");
-    }
-  }
-}
-
-function getCurrentBoard() {
-  var oReq = new XMLHttpRequest();
-  oReq.onload = function() {
-    var boardInfo = JSON.parse(this.responseText);
-    if (boardInfo.marks !== null) {
-      setNewBoard(boardInfo.marks);
-    }
-    enableOptions();
-  };
-  oReq.open("GET", "api/board", true);
-  oReq.send();
-}
-
-function sendMove(move) {
-  var oReq = new XMLHttpRequest();
-  oReq.onload = function() {
-    getCurrentBoard();
-  };
-  oReq.open("POST", "api/make-move", true);
-  oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  oReq.send("move="+move);
-}
-
-function makeOnClick(optEl) {
-  var index = optEl.value;
-  return function() {
-    disableOptions();
-    sendMove(index);
-  };
-}
-
-// var submitBtn = document.getElementById("submit-btn");
-// submitBtn.classList.add("hidden");
-
-// var options = [];
-
-// for (var i = 0; i < 9; i++) {
-//   var opt = document.getElementById("move"+i);
-//   options.push(opt);
-//   opt.onclick = makeOnClick(opt);
-// }
+var board = new Board(new API(), submitBtn, options);
+board.prepareBoard();
