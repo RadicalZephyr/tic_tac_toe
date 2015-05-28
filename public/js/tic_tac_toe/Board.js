daa0ee89268208a92e72a155cc6eb461c45d28b4
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-28 12:14:42 -0500

    Move API class and specs into their own files

--- public/js/tic_tac_toe/Board.js
+++ public/js/tic_tac_toe/Board.js
@@ -1,22 +1,3 @@
-function API() {
-
-}
-
-API.prototype.getBoard = function(callback) {
-    var oReq = new XMLHttpRequest();
-    oReq.onload = callback;
-    oReq.open("GET", "api/board", true);
-    oReq.send();
-};
-
-API.prototype.sendMove = function(move, callback) {
-    var oReq = new XMLHttpRequest();
-    oReq.onload = callback;
-    oReq.open("POST", "api/make-move", true);
-    oReq.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    oReq.send("move="+move);
-};
-
 function Board(api, submitBtn, options) {
     this.api = api;
     this.submitBtn = submitBtn;
