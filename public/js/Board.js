1f08af367d2fa01b876b7cec2f7af330dcda99c6
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-28 10:24:19 -0500

    Add implementation for hideButton

--- public/js/Board.js
+++ public/js/Board.js
@@ -8,5 +8,10 @@ Board.prototype.prepareBoard = function() {
     this.options.forEach(function(item) { this.setupOption(item); }, this);
 };
 
-Board.prototype.hideButton = function(btn) {};
-Board.prototype.setupOption = function(option) {};
+Board.prototype.hideButton = function(btn) {
+    btn.classList.add("hidden");
+};
+
+Board.prototype.setupOption = function(option) {
+
+};
