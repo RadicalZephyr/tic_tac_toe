0bf21c9f674aeede50f51e361e9d38685e9d52f5
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-28 10:44:03 -0500

    Add tests for enabling and disabling all options

--- spec/javascripts/BoardSpec.js
+++ spec/javascripts/BoardSpec.js
@@ -10,4 +10,25 @@ describe("Board", function() {
         expect(board.hideButton).toHaveBeenCalledWith(submitBtn);
         expect(board.setupOption).toHaveBeenCalledWith(options[0]);
     });
+
+    it("can disable all the options", function() {
+        var submitBtn = {};
+        var opt1 = {"disable": function() {}};
+        var options = [opt1];
+        var board = new Board(submitBtn, options);
+        spyOn(opt1, "disable");
+        board.disableOptions();
+        expect(opt1.disable).toHaveBeenCalled();
+    });
+
+    it("can enable all the options", function() {
+        var submitBtn = {};
+        var opt1 = {"enable": function() {}};
+        var options = [opt1];
+        var board = new Board(submitBtn, options);
+        spyOn(opt1, "enable");
+        board.enableOptions();
+        expect(opt1.enable).toHaveBeenCalled();
+    });
+
 });
