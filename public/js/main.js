82688a3509064794d2c9ec840ac2b4d09acd9ac3
Zefira Shannon <earthlingzephyr@gmail.com>
2015-05-27 22:45:42 -0500

    Spike out the js functionality

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,40 @@
-alert("Hello JS World!");
+var submitBtn = document.getElementById("submit-btn");
+submitBtn.classList.add("hidden");
+
+var options = [];
+
+for (var i = 0; i < 9; i++) {
+  options.push(document.getElementById("move"+i));
+}
+
+function disableOptions() {
+  for (var i = 0; i < 9; i++) {
+    options[i].disabled = true;
+  }
+}
+
+function enableOptions() {
+  for (var i = 0; i < 9; i++) {
+    options[i].disabled = false;
+  }
+}
+
+function setNewBoard(board) {
+  for (var i = 0; i < 9; i++) {
+    if (board[i] !== " ") {
+      options[i].parentElement.classList.add(board[i].toUpperCase()+"-marker");
+    }
+  }
+}
+
+function getCurrentBoard() {
+  var oReq = new XMLHttpRequest();
+  oReq.onload = function() {
+    var boardInfo = JSON.parse(this.responseText);
+    if (boardInfo.marks !== null) {
+      setNewBoard(boardInfo.marks);
+    }
+  };
+  oReq.open("GET", "api/board", true);
+  oReq.send();
+}
