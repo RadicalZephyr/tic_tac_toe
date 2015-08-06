6911eb12fa3129a09a2695f6128c49b2772642cc
Zefira Shannon <earthlingzephyr@gmail.com>
2015-08-06 16:27:14 -0500

    Remove all web specific code

--- lib/rack_tac_toe.rb
+++
@@ -1,4 +0,0 @@
-require 'tic_tac_toe_gs'
-require 'tic_tac_toe_gs/web/rack_shell'
-require 'tic_tac_toe_gs/web/router'
-require 'tic_tac_toe_gs/web/views'
--- spec/tic_tac_toe/web/views/home_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-require 'tic_tac_toe_gs/web/views/home'
-
-describe TicTacToeGS::Web::Views::Home do
-
-end
