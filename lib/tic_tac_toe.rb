ca85942a9bdfcab13fcbd290608a5152efe05e50
Geoff Shannon <geoffpshannon@gmail.com>
2015-08-06 14:56:15 -0500

    Remove CLI interface specific code

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,5 +1,4 @@
 require "tic_tac_toe/version"
-require "tic_tac_toe/console/console_shell"
 require "tic_tac_toe/core/board"
 require "tic_tac_toe/core/mark"
 require "tic_tac_toe/core/game"
@@ -9,8 +8,4 @@ require "tic_tac_toe/core/players/minimax"
 
 module TicTacToe
 
-  def TicTacToe.run
-    shell = TicTacToe::Console::ConsoleShell.new_shell(IO::console)
-    shell.main_loop
-  end
 end
